37cbd63e5e785f155d2c6f6d0f3077cee1eb8bcc
s24675 <s24675@pjwstk.edu.pl>
2024-04-18 19:02:03 +0200

    Dokonano poprawek. Podłączono SqlClient.

--- APBD_RestAPI_SQL/obj/Debug/net8.0/APBD_RestAPI_SQL.AssemblyInfo.cs
+++ APBD_RestAPI_SQL/obj/Debug/net8.0/APBD_RestAPI_SQL.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APBD_RestAPI_SQL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac05455ae712e14a7d5aac889b3e8f384981af30")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d8e16dcdd751f9de2a2eaa4fed8ea18a2c8901")]
 [assembly: System.Reflection.AssemblyProductAttribute("APBD_RestAPI_SQL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APBD_RestAPI_SQL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
